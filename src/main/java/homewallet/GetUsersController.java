719d6cb0e6799bd9ae70a367a319c755b3d8b851
Falzone Michele <michele.falzone@ti8m.ch>
2018-03-28 13:49:45 +0200

    first controller and spring app

---
+++ src/main/java/homewallet/GetUsersController.java
@@ -0,0 +1,18 @@
+package homewallet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GetUsersController {
+
+    @GetMapping("/getusers")
+    public String getusers(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        // return the view stored on templates
+        return "getusers";
+    }
+
+}
