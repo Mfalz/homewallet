7c40846ca0b33da50ae4c1cfecd0d961048ed57a
Falzone Michele <michele.falzone@ti8m.ch>
2018-08-18 10:32:46 +0200

    static layout installed

--- static/src/app/app.module.ts
+++ static/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
